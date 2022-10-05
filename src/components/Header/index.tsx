498e346a8a404242a8d9c0ea757263e9cc9de979
JorgeFPeres <jorge-peres@live.com>
2022-10-05 15:19:39 -0300

    feat: transaction page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Logo } from '../../assets/logo'
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <Logo />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
